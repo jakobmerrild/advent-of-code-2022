b7cbaf546a2385a351aeee6ab318ad30118a96e8
Jakob Merrild <jm@famly.co>
2022-12-03 16:10:35 +0100

    Apply formatting

--- src/main/scala/app/AppOpts.scala
+++ src/main/scala/app/AppOpts.scala
@@ -4,5 +4,6 @@ import com.monovore.decline.Opts
 
 object AppOpts {
   final case class Solve(day: Int)
-  lazy val solve: Opts[Solve] = Opts.subcommand("solve", "solves the challenge for the given day")(Opts.argument[Int]("day").map(Solve.apply))
+  lazy val solve: Opts[Solve] =
+    Opts.subcommand("solve", "solves the challenge for the given day")(Opts.argument[Int]("day").map(Solve.apply))
 }
